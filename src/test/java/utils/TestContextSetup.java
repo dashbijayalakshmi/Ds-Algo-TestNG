b15c5385104b76bfadd71201cf6a0868fd45f301
Bijayalakshmi Dash <dashbijayalakshmi@gmail.com>
2024-11-06 11:51:10 -0700

    utils package update

---
+++ src/test/java/utils/TestContextSetup.java
@@ -0,0 +1,19 @@
+package utils;
+
+import org.openqa.selenium.WebDriver;
+
+import Pageobjects.PageObjectManger;
+
+public class TestContextSetup {
+public WebDriver driver;
+public PageObjectManger pageobjectmanager;
+public ConfigReader configreader;
+public DriverFactory driverfactory;
+
+public TestContextSetup() throws Throwable {
+	//pageobjectmanager=new PageObjectManger(testbase.WebDriverManager());
+	driverfactory=new DriverFactory();
+	String browser = configreader.getBrowserType();
+	pageobjectmanager=new PageObjectManger(driverfactory.initializeDrivers(browser));
+}
+}
